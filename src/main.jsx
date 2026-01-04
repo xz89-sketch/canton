52956fc8c998e52d4f70ed785b60c19e89a3d623
Shane <x.z89@outlook.com>
2026-01-04 16:38:26 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,15 @@
+
+
+// src/main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './App'; // 引入你的主组件
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+// 找到 HTML 中的 <div id="root"></div>
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// 渲染 App 组件
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
